012220f8eeb3e1b220951eb9c2144c5e783a902d
Eric Freeman <efreeman@1800contacts.com>
2015-06-11 17:03:39 -0600

    Added second level with spring

---
+++ Assets/Scripts/Spring.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Spring : MonoBehaviour
+    {
+
+        void OnTriggerEnter(Collider collision)
+        {
+            if (collision.name == "Player")
+            {
+                collision.GetComponent<Rigidbody>().AddForce(0, 666, 0);
+            }
+        }
+    }
+}
