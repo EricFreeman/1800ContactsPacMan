da8712cce8f4336c5dbb616f80f8d60f70b2a871
Eric Freeman <efreeman@1800contacts.com>
2015-06-12 16:53:35 -0600

    Instead of procedurally generated levels, we are going to do handcrafted levels.  Added moving platform, and also contacts box as a MacGuffin victory condition.

---
+++ Assets/Scripts/BoxOfContacts.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoxOfContacts : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.name == "Player")
+        {
+            Debug.Log("VICTORY");
+        }
+    }
+}
---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class MovingPlatform : MonoBehaviour
+    {
+        public Vector3 StartPosition;
+        public Vector3 EndPosition;
+        public float PlatformSpeed;
+
+        private bool _movingTo;
+
+        void Update()
+        {
+            if (_movingTo)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, EndPosition, PlatformSpeed*Time.deltaTime);
+                if (transform.position == EndPosition)
+                {
+                    _movingTo = false;
+                }
+            }
+            else
+            {
+                transform.position = Vector3.MoveTowards(transform.position, StartPosition, PlatformSpeed*Time.deltaTime);
+                if (transform.position == StartPosition)
+                {
+                    _movingTo = true;
+                }
+            }
+        }
+
+        void OnCollisionEnter(Collision collision)
+        {
+            var col = collision.collider.transform;
+            if (col.name == "Player")
+            {
+                col.SetParent(transform);
+            }
+        }
+
+        void OnCollisionExit(Collision collision)
+        {
+            var col = collision.collider.transform;
+            if (col.name == "Player")
+            {
+                col.SetParent(null);
+            }
+        }
+    }
+}
