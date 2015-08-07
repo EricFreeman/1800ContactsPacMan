e40fa965d37b0fc581ff77d50b3f3ee934bf5ee3
Eric Freeman <efreeman@1800contacts.com>
2015-08-07 11:31:22 -0600

    More work on main menu.

---
+++ Assets/Scripts/UI/LevelSelectButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.UI
+{
+    public class LevelSelectButton : MonoBehaviour
+    {
+        public string LevelName;
+        public Text Text;
+
+        void Start()
+        {
+            Text.text = LevelName;
+        }
+
+        public void SelectLevel()
+        {
+            Application.LoadLevel("Game");
+        }
+    }
+}
