b3bca2bfaf5e0486c903fc436b2b3c0a2f9dde05
Kevin Owens <kowens@gmail.com>
2015-08-28 11:01:19 -0600

    Added Speed Power Up prefab

---
+++ Assets/Scripts/Spinner.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+	public class Spinner : MonoBehaviour
+	{
+		public float Speed;
+
+		void Update()
+		{
+			transform.Rotate(Vector3.forward * Speed * Time.deltaTime);
+		}
+	}
+}
