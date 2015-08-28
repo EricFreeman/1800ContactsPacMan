0239caa6109f299ffa3dc310c5a593b571e2c0fd
Kevin Owens <kowens@gmail.com>
2015-08-28 13:06:53 -0600

    Adding a new level, "You've Got a Lot of Optic Nerve"

---
+++ Assets/Scripts/Accelerator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+	public class Accelerator : MonoBehaviour
+	{
+		void OnTriggerEnter(Collider collision)
+		{
+			if (collision.name == "Player")
+			{
+				collision.GetComponent<Rigidbody>().AddForce(transform.forward * 666);
+			}
+		}
+	}
+}
