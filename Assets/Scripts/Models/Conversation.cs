b0b2c1f9abe4457e25608ba44ee74dc64b2c4324
Eric Freeman <efreeman@1800contacts.com>
2015-08-27 23:30:57 -0600

    Holy crap the conversations are basically working.  Wacky.

--- Assets/Scripts/Models/Conversation.cs
+++ Assets/Scripts/Models/Conversation.cs
@@ -5,5 +5,11 @@ namespace Assets.Scripts.Models
     public class Conversation
     {
         public List<Line> Lines;
+        public int CurrentLine;
+
+        public bool IsComplete
+        {
+            get { return CurrentLine >= Lines.Count; }
+        }
     }
 }
--- Assets/Scripts/UI/ConversationManager.cs
+++ Assets/Scripts/UI/ConversationManager.cs
@@ -1,24 +1,38 @@
-﻿using Assets.Scripts.Managers;
+﻿using System.Collections.Generic;
+using Assets.Scripts.Managers;
 using Assets.Scripts.Models;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Assets.Scripts.UI
 {
     public class ConversationManager : MonoBehaviour
     {
         public TypewriterText Typewriter;
+        public Text SpeakerName;
+        public List<Image> Images;
+        public AudioSource AudioSource;
 
         private Conversation _conversation;
 
         void Start()
         {
             var manager = new XmlManager<Conversation>();
-            _conversation = manager.Load("Assets/Conversations/Level1.txt");
+            _conversation = manager.Load("Assets/Conversations/Level3.txt");
         }
 
         void Update()
         {
-            
+            if (!AudioSource.isPlaying && !_conversation.IsComplete)
+            {
+                var conversation = _conversation.Lines[_conversation.CurrentLine];
+                Typewriter.TypeText(conversation.Text);
+                SpeakerName.text = conversation.Name;
+                AudioSource.clip = Resources.Load<AudioClip>("Audio/" + conversation.Audio);
+                AudioSource.Play();
+
+                _conversation.CurrentLine++;
+            }
         }
     }
 }
