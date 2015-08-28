b9a4a247daf1c126ea862580f1253fe53654dc98
Kevin Owens <kowens@gmail.com>
2015-08-28 16:42:13 -0600

    For level 1, change the moving platform to use Admiral's geometry.

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -1,33 +1,56 @@
-﻿using UnityEngine;
+﻿using UnityEditorInternal;
+using UnityEngine;
 
 namespace Assets.Scripts
 {
-    public class MovingPlatform : MonoBehaviour
+	public class MovingPlatform : MonoBehaviour
     {
         public Vector3 StartPosition;
         public Vector3 EndPosition;
         public float PlatformSpeed;
+		public float IdleSeconds;
 
-        private bool _movingTo;
+		private MovingPlatformState _movingPlatformState = MovingPlatformState.IdlingAtStartPosition;
+		private float _timeToStopIdling;
+
+		void Start()
+		{
+			_timeToStopIdling = Time.fixedTime + IdleSeconds;
+		}
 
         void Update()
         {
-            if (_movingTo)
-            {
-                transform.position = Vector3.MoveTowards(transform.position, EndPosition, PlatformSpeed * Time.deltaTime);
-                if (transform.position == EndPosition)
-                {
-                    _movingTo = false;
-                }
-            }
-            else
-            {
-                transform.position = Vector3.MoveTowards(transform.position, StartPosition, PlatformSpeed * Time.deltaTime);
-                if (transform.position == StartPosition)
-                {
-                    _movingTo = true;
-                }
-            }
+	        switch (_movingPlatformState)
+	        {
+		        case MovingPlatformState.IdlingAtStartPosition:
+			        if (Time.fixedTime > _timeToStopIdling)
+			        {
+				        _movingPlatformState = MovingPlatformState.MovingToEndPosition;
+			        }
+			        break;
+				case MovingPlatformState.IdlingAtEndPosition:
+					if (Time.fixedTime > _timeToStopIdling)
+					{
+						_movingPlatformState = MovingPlatformState.MovingToStartPosition;
+					}
+			        break;
+				case MovingPlatformState.MovingToEndPosition:
+					transform.position = Vector3.MoveTowards(transform.position, EndPosition, PlatformSpeed * Time.deltaTime);
+					if (transform.position == EndPosition)
+					{
+						_movingPlatformState = MovingPlatformState.IdlingAtEndPosition;
+						_timeToStopIdling = Time.fixedTime + IdleSeconds;
+					}
+			        break;
+				case MovingPlatformState.MovingToStartPosition:
+					transform.position = Vector3.MoveTowards(transform.position, StartPosition, PlatformSpeed * Time.deltaTime);
+					if (transform.position == StartPosition)
+					{
+						_movingPlatformState = MovingPlatformState.IdlingAtStartPosition;
+						_timeToStopIdling = Time.fixedTime + IdleSeconds;
+					}
+			        break;
+	        }
         }
 
         void OnDrawGizmos()
@@ -55,4 +78,12 @@ namespace Assets.Scripts
             }
         }
     }
+
+	public enum MovingPlatformState
+	{
+		IdlingAtStartPosition,
+		MovingToEndPosition,
+		MovingToStartPosition,
+		IdlingAtEndPosition
+	}
 }
