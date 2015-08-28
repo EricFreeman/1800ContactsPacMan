6dcf44bb172f257ef008f1230657e8aa0bd7d561
Eric Freeman <efreeman@1800contacts.com>
2015-08-28 10:58:20 -0600

    Made level manager much more dynamic and swagalicious.

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,31 +1,115 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Assets.Scripts.Messages;
+using UnityEngine;
+using UnityEventAggregator;
 
 namespace Assets.Scripts
 {
-    public class LevelManager : MonoBehaviour
+    public class LevelManager : MonoBehaviour, IListener<LoadNextLevelMessage>
     {
-        public GameObject CurrentLevel;
+        [HideInInspector]
+        public List<LevelSequence> LevelSequence;
+        public bool IgnoreLevelLoad;
+
+        private GameObject _currentLevel;
 
         void Start()
         {
-            string level = PlayerPrefs.GetString("Level");
-            if (string.IsNullOrEmpty(level))
+            LevelSequence = InitializeLevelSequence();
+            var level = PlayerPrefs.GetString("Level");
+
+            if (!IgnoreLevelLoad)
             {
-                // TODO: Get rid of hard coded level1
-                level = "Playground";
+                if(string.IsNullOrEmpty(level))
+                    LoadNextLevel();
+                else
+                    LoadLevel(level);
             }
 
-            LoadLevel(level);
+            this.Register<LoadNextLevelMessage>();
+        }
+
+        void OnDestroy()
+        {
+            this.UnRegister<LoadNextLevelMessage>();
         }
 
         public void LoadLevel(string levelName)
         {
-            if (CurrentLevel != null)
+            if (_currentLevel != null)
             {
-                Destroy(CurrentLevel);
+                Destroy(_currentLevel);
             }
 
-            CurrentLevel = Instantiate(Resources.Load<GameObject>("Prefabs/Levels/" + levelName));
+            _currentLevel = Instantiate(Resources.Load<GameObject>("Prefabs/Levels/" + levelName));
         }
+
+        public void LoadCutscene(string cutsceneName, string levelName)
+        {
+            PlayerPrefs.SetString("Cutscene", cutsceneName);
+            PlayerPrefs.SetString("Level", levelName);
+            Application.LoadLevel("Cutscene");
+        }
+
+        private List<LevelSequence> InitializeLevelSequence()
+        {
+            return new List<LevelSequence>
+            {
+                new LevelSequence { PrefabName="Level1", IsCutscene = true },
+                new LevelSequence { PrefabName="Level1", DisplayName = "Breaking the Eyes 1"},
+                new LevelSequence { PrefabName="Level2", IsCutscene = true },
+                new LevelSequence { PrefabName="Level2", DisplayName = "Breaking the Eyes 2" },
+                new LevelSequence { PrefabName="Level3", IsCutscene = true },
+                new LevelSequence { PrefabName="Level3", DisplayName = "Eye Am Watching You" },
+                new LevelSequence { PrefabName="Level4", IsCutscene = true },
+                new LevelSequence { PrefabName="Level4", DisplayName = "Between The H and J" },
+                new LevelSequence { PrefabName="Level5", IsCutscene = true },
+                new LevelSequence { PrefabName="Level5", DisplayName = "My Own Prism" },
+                new LevelSequence { PrefabName="Level6", IsCutscene = true },
+                new LevelSequence { PrefabName="Level6", DisplayName = "Last Sighting" },
+                new LevelSequence { PrefabName="Finale", IsCutscene = true },
+            };
+        }
+
+        private void LoadNextLevel()
+        {
+            var currentLevel = PlayerPrefs.GetString("Level");
+            if (string.IsNullOrEmpty(currentLevel))
+            {
+                LoadLevel(LevelSequence.First(x => !x.IsCutscene).PrefabName);
+            }
+            else
+            {
+                var index = LevelSequence.IndexOf(LevelSequence.FirstOrDefault(x => x.PrefabName == currentLevel && !x.IsCutscene));
+                if (index >= LevelSequence.Count)
+                {
+                    Application.LoadLevel("MainMenu");
+                }
+
+                var next = LevelSequence[index + 1];
+                if (next.IsCutscene && index + 2 < LevelSequence.Count)
+                {
+                    var nextLevel = LevelSequence[index + 2];
+                    LoadCutscene(next.PrefabName, nextLevel.PrefabName);
+                }
+                else
+                {
+                    LoadLevel(next.PrefabName);
+                }
+            }
+        }
+
+        public void Handle(LoadNextLevelMessage message)
+        {
+            LoadNextLevel();
+        }
+    }
+
+    public class LevelSequence
+    {
+        public string PrefabName;
+        public string DisplayName;
+        public bool IsCutscene;
     }
 }
