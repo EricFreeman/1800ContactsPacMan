e0ddcc53b7f1083d69db68491baa821a46b7df68
Eric Freeman <efreeman@1800contacts.com>
2015-08-27 22:51:06 -0600

    A whole bunch of conversations

---
+++ Assets/Scripts/UI/ConversationManager.cs
@@ -0,0 +1,17 @@
+﻿using Assets.Scripts.Models;
+using UnityEngine;
+
+namespace Assets.Scripts.UI
+{
+    public class ConversationManager : MonoBehaviour
+    {
+        public TypewriterText Typewriter;
+
+        private Conversation _conversation;
+
+        void Update()
+        {
+            
+        }
+    }
+}
