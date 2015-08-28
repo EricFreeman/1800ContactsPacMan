affdc7acd9cf2edef8e0db08735fb89d2a7803dc
Kevin Owens <kowens@gmail.com>
2015-08-28 10:01:21 -0600

    Added exploding lasers to level 6

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+	public class Laser : MonoBehaviour
+	{
+		public GameObject Explosion;
+
+		private float _concentrationSpeed = 0.3f;
+		
+		void Update()
+		{
+			var scaleReduction = _concentrationSpeed*Time.deltaTime;
+			transform.localScale -= new Vector3(scaleReduction, scaleReduction, scaleReduction);
+			if (transform.localScale.x <= 0)
+			{
+				Explode();
+			}
+		}
+
+		private void Explode()
+		{
+			Instantiate(Explosion, transform.position, Quaternion.identity);
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/LaserSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Assets.Scripts
+{
+	public class LaserSpawner : MonoBehaviour
+	{
+		public GameObject Laser;
+		public float MinimumSpawnTime;
+		public float MaximumSpawnTime;
+
+		private float _nextSpawnTime;
+
+		void Start()
+		{
+			SetNextSpawnTime();
+		}
+
+		private void SetNextSpawnTime()
+		{
+			var randomInterval = Random.Range(MinimumSpawnTime, MaximumSpawnTime);
+			_nextSpawnTime = Time.fixedTime + randomInterval;
+		}
+
+		void Update()
+		{
+			if (Time.fixedTime >= _nextSpawnTime)
+			{
+				Instantiate(Laser, transform.position, Quaternion.identity);
+				SetNextSpawnTime();
+			}
+		}
+	}
+}
