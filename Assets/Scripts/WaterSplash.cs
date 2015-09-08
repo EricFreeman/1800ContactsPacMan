33e6d7c3a134cc65c3baee096c9e569dd3c16213
Kevin Owens <kowens@gmail.com>
2015-09-07 22:55:26 -0600

    Make a splash sound when you hit the water.

---
+++ Assets/Scripts/WaterSplash.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class WaterSplash : MonoBehaviour
+    {
+        void OnTriggerEnter(Collider collision)
+        {
+            if (collision.name == "Player")
+            {
+                var audioClip = (AudioClip)Resources.Load("Audio/splash", typeof(AudioClip));
+                AudioSource.PlayClipAtPoint(audioClip, transform.position);
+            }
+        }
+    }
+}
